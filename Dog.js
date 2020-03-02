f2330069b934dccedfe38d93ef7fe48e43e01c51
= <=>
2020-03-02 11:56:31 +0700

    sayHi method for Dog

--- Dog.js
+++ Dog.js
@@ -1,7 +1,18 @@
-function Dog(){
+var chalk = require('chalk')
+
+function Dog(name){
+	this.name = name
 	this.stamach =[];
 }
 
 Dog.prototype.eat = function(cat){
 	this.stamach.push(cat);
 }
+
+Dog.prototype.sayHi = function(){
+	console.log('go go go ' + chalk.blue(this.name));
+}
+
+
+
+module.exports = Dog;
